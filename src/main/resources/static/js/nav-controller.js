a9b1423b9a6295f175e5ef9e39a9458e1b160c37
Bryce J. Fisher <Bryce.Fisher@mrm-mccann.com>
2016-08-10 14:24:45 -0600

    Annotated Angular Injections to prepare js for minification.

--- src/main/resources/static/js/nav-controller.js
+++ src/main/resources/static/js/nav-controller.js
@@ -1,60 +1,52 @@
-angular.module('navController', [])
-	.controller('nav', function($scope, $state) {
+(function() {
+	console.log("Loading navController...");
+	angular.module('navController', []).controller('nav', navController);
+
+	navController.$inject = [ '$scope', '$state' ];
+	function navController($scope, $state) {
 		$scope.title = 'Project name';
 
 		// returns true if the current router url matches the passed in url
 		// so views can set 'active' on links easily
 		$scope.isUrl = function(url) {
-			if (url === '#') return false;
+			if (url === '#')
+				return false;
 			return ('#' + $state.$current.url.source + '/').indexOf(url + '/') === 0;
 		};
 
-		$scope.pages = [
-			{
-				name: 'Home',
-				url: '#/'
-			},
-			{
-				name: 'About',
-				url: '#/about'
-			},
-			{
-				name: 'Contact',
-				url: '#/contact'
-			},
-			{
-				name: 'Theme Example',
-				url: '#/theme'
-			},
-			{
-				name: 'Blog',
-				url: '#/blog'
-			},
-			{
-				name: 'Grid',
-				url: '#/grid'
-			},
-			{
-				name: 'UI',
-				url: '#/ui'
-			},
-			{
-				name: 'Dropdown Example',
-				url: '#',
-				subPages: [
-					{
-						name: 'About',
-						url: '#/about'
-					},
-					{},
-					{
-						name: 'Header',
-					},
-					{
-						name: 'Contact',
-						url: '#/contact'
-					}
-				]
-			}
-		]
-	});
+		$scope.pages = [ {
+			name : 'Home',
+			url : '#/'
+		}, {
+			name : 'About',
+			url : '#/about'
+		}, {
+			name : 'Contact',
+			url : '#/contact'
+		}, {
+			name : 'Theme Example',
+			url : '#/theme'
+		}, {
+			name : 'Blog',
+			url : '#/blog'
+		}, {
+			name : 'Grid',
+			url : '#/grid'
+		}, {
+			name : 'UI',
+			url : '#/ui'
+		}, {
+			name : 'Dropdown Example',
+			url : '#',
+			subPages : [ {
+				name : 'About',
+				url : '#/about'
+			}, {}, {
+				name : 'Header',
+			}, {
+				name : 'Contact',
+				url : '#/contact'
+			} ]
+		} ]
+	}
+})();
