5999a6e08de845060255886dbe467e728877f9c3
Bryce J. Fisher <Bryce.Fisher@mrm-mccann.com>
2016-08-10 13:40:20 -0600

    Initial commit.

---
+++ src/main/java/com/checker/ResourceCheckerApplication.java
@@ -0,0 +1,26 @@
+package com.checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class ResourceCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceCheckerApplication.class, args);
+	}
+	
+	@Bean
+    public WebMvcConfigurerAdapter forwardToIndex() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName(
+                        "forward:/index.html");
+            }
+        };
+    }
+}
